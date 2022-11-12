3700078629692382d82f403204a39aaa04cc964b
syspuke <cameronpoe@protonmail.com>
2022-11-12 02:05:08 -0600

    banner, fixes, color

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def init():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--ip', type=str, required=True, help="IP Address")
+    parser.add_argument('--cwd', type=str, help="Working Directory")
 
     args = parser.parse_args()
     
@@ -29,7 +30,7 @@ def nmap(host):
 
     resultout = f"nmap_{hex_uuid()}"
 
-    cmdnmap = f"nmap -sC -sV {host} -oA nmap/{resultout}"
+    cmdnmap = f"nmap -sC -sV -oA nmap/{resultout} {host}"
 
     logging.info(f'Running...{cmdnmap}')
     exec_cmd(cmdnmap)
@@ -38,21 +39,29 @@ def nmap(host):
 
     services_list = parse_nmap_file(xml_path)
 
-    for service in services_list:
-        print(service.split(":"))
-
 
 def ffuf_dir_enum(host, port):
     exec_cmd("mkdir -p ffuf")
     resultout = f"ffuf_{hex_uuid()}"
     cmdffuf = f"ffuf -w /usr/share/seclists/Discovery/Web-Content/big.txt -o ffuf/{resultout}.md -fc 302 -u http://{host}:{port}/FUZZ"
 
-    logging.info(f'Running...{cmdffuf}')
+    logging.info(f'[HTTP DETECTED] Running...{cmdffuf}')
     print(exec_cmd(cmdffuf))
 
 
+
+def nmap_smb_enum(host, port):
+    exec_cmd("mkdir -p nmap")
+    resultout = f"smb_{hex_uuid()}"
+    cmdnmap = f"nmap --script=smb-enum-shares.nse,smb-enum-users.nse -p {port} -oA nmap/{resultout} {host}"
+
+    logging.info(f'[SMB DETECTED] Running...{cmdnmap}')
+    print(exec_cmd(cmdnmap))
+
 def parse_nmap_file(path_xml):
     nmap_report = NmapParser.parse_fromfile(path_xml)
+
+    services = []
 
     for host in nmap_report.hosts:
         if len(host.hostnames):
@@ -60,35 +69,41 @@ def parse_nmap_file(path_xml):
         else:
             tmp_host = host.address
 
-
-    services = []
-
-    for serv in host.services:
-        services.append(f"{serv.service}:{serv.port}")
+        for serv in host.services:
+            services.append(f"{serv.service}:{serv.port}")
     
     
     for service in services:
-        print(service)
         service = service.split(":")
         if service[0] == "http":
-            ffuf_dir_enum("10.129.15.31",service[1])
+            ffuf_dir_enum(host.address,service[1])
+        elif service[0] == "netbios-ssn":
+            nmap_smb_enum(host.address,service[1])
+
 
     return services
 
 
+def banner():
+    print(Fore.RED + '''
+
+███████╗███╗░░██╗██╗░░░██╗███╗░░░███╗██████╗░██╗░░░██╗██╗░░██╗███████╗
+██╔════╝████╗░██║██║░░░██║████╗░████║██╔══██╗██║░░░██║██║░██╔╝██╔════╝
+█████╗░░██╔██╗██║██║░░░██║██╔████╔██║██████╔╝██║░░░██║█████═╝░█████╗░░
+██╔══╝░░██║╚████║██║░░░██║██║╚██╔╝██║██╔═══╝░██║░░░██║██╔═██╗░██╔══╝░░
+███████╗██║░╚███║╚██████╔╝██║░╚═╝░██║██║░░░░░╚██████╔╝██║░╚██╗███████╗
+╚══════╝╚═╝░░╚══╝░╚═════╝░╚═╝░░░░░╚═╝╚═╝░░░░░░╚═════╝░╚═╝░░╚═╝╚══════╝
+
+                        [Enumpuke 0.0.1]
+    ''' + Fore.RESET)
+
 def main():
     args = init()
     cwd = os.getcwd()
-    global input_cwd
-    input_cwd = input(f"Use {cwd} as working directory? (yes or no)")
-
-    if input_cwd != "yes":
-        input_cwd = input(f"Enter the work directory path. ")
-    else:
-        input_cwd = cwd
+    banner()
     
-    #print(nmap(args.ip))
-    parse_nmap_file("nmap/nmap_58ef8adb86194a34b4ecf1f6222598f1.xml")
+    nmap(args.ip)
+    #parse_nmap_file("nmap/nmap_58ef8adb86194a34b4ecf1f6222598f1.xml")
 
 
 main()
